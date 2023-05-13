fa9f8071071d26e36cce6414b24b308ca3344495
hao0516hsu <chhsu0516@gmail.com>
2023-05-13 14:55:42 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const exphbs = require("express-handlebars")
 const bodyParser = require("body-parser")
-const mongoose = require("mongoose")
 
 const routes = require('./routes/index')
+require('./config/mongoose')
 const app = express()
 
 app.engine('handlebars', exphbs({ defaultLayout: "main" }))
@@ -11,22 +11,6 @@ app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(routes)
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGODB_URI)
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected')
-})
 
 app.listen(3000, () => {
   console.log(`App is listening to http://localhost:3000`)
--- models/seeds/userSeeder.js
+++ models/seeds/userSeeder.js
@@ -1,22 +1,8 @@
-const mongoose = require('mongoose')
+const db = require('../../config/mongoose')
 const User = require('../user_account')
 const userList = require('../user.json')
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGODB_URI)
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error')
-})
-
 db.once('open', () => {
-  console.log('mongodb connected')
-
   userList.forEach(user => {
     User.create({
       firstName: user.firstName,
@@ -24,6 +10,5 @@ db.once('open', () => {
       password: user.password
     })
   })
-  
   console.log('done')
 })
